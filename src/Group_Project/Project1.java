757d69258ee512a8d6d89506029d21aa46a1ba7a
majon92 <jordanmatt92@gmail.com>
2021-02-11 20:12:17 +0000

    Initial commit

---
+++ src/Group_Project/Project1.java
@@ -0,0 +1,11 @@
+package Group_Project;/**
+* Created by stevejobs on 11/02/2021
+* COMMENTS ABOUT PROGRAM HERE 
+**/public class Project1{
+
+   public static void main (String[]args)
+   {
+   
+   
+   }//main
+}//class  
